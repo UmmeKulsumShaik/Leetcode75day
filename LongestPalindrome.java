1428150f8a75d6ad8707206f7132f17e675d5567
UmmeKulsumShaik <115888799+UmmeKulsumShaik@users.noreply.github.com>
2023-03-13 22:12:09 +0530

    Create LongestPalindrome.java

---
+++ LongestPalindrome.java
@@ -0,0 +1,16 @@
+# Leetcode75day
+class Solution{
+public int longestPalindrome(String s){
+int[] charcounts=new int[128];
+for(char c: s.toCharArray()){
+char_counts[c]++;
+}
+int result=0;
+for(Integer char_count : char_counts){
+result+=char_count/2*2;
+if(result%2==0 && char_count %2==1){
+result+=1;
+}
+}
+return result;
+)
